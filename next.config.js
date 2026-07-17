67dd3ca413ae7d9dceaa9e7469808b48810fab47
architvermacn-create <archit.verma.cn@snapdeal.com>
2026-07-17 10:15:57 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'replicate.delivery' },
+      { protocol: 'https', hostname: 'pbxt.replicate.delivery' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
+    ],
+  },
+};
+module.exports = nextConfig;
